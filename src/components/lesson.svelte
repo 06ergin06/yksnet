129437c32f66600e45aa8548d22d134a546e291a
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2023-12-09 22:52:35 +0300

    .

--- src/components/lesson.svelte
+++ src/components/lesson.svelte
@@ -11,7 +11,7 @@
 </script>
 <div class="my-5">
     <h3 class="text-xl">{name}</h3>
-    <input placeholder="Doğru" on:change={netHesap} bind:value={saltdogru} class="input" type="text">
+    <input placeholder="Doğru" bind:value={saltdogru} class="input" type="text">
     <input placeholder="Yanlış" on:change={netHesap} bind:value={saltyanlis} class="input" type="text">
     <div class="border-2 border-gray-800 rounded px-2">
         {net} net
