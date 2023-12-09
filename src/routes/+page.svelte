4dfb7f80629f3bc6ca393ede0ceeefb886e4d1fe
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2023-12-09 22:27:42 +0300

    0.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,10 @@
     import Lesson from '../components/lesson.svelte';
 
 </script>
+
 <header class="center">
-    <h1>YKS Net Hesaplama</h1>
-    <div class="box">
+    <h1 class="md:m-0 my-5 ">YKS Net Hesaplama</h1>
+    <div class="box md:m-0 my-5 ">
         <h3>Aşağıya doğrularınızı ve yanlışlarınızı girebilirsiniz.</h3>
         <Lesson name="Türkçe" />
         <Lesson name="Sosyal Bilimler" />
@@ -12,4 +13,3 @@
         <Lesson name="Fen Bilimleri" />
     </div>
 </header>
-
